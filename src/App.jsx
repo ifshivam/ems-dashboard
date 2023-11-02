ae9e89416bce712b80e59f771f8abe7e3b0ba56e
shivam.rai <shivam.rai@loblaw.ca>
2023-11-02 05:38:22 +0530

    list Employee features implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
 
 function App() {
 
 
   return (
     <>
-    
+    <ListEmployeeComponent/>
     </>
   )
 }
